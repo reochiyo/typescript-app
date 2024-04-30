3cf15a7d65a9fdaadd79803320a2d80f509e4ed2
ReoChiyomaru <reo.chiyomaru@gmail.com>
2024-05-01 03:03:22 +0900

    MySQLとbackendを同期

--- backend/app.ts
+++ backend/app.ts
@@ -1,21 +1,35 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { MyTwitter } from "./types";
+import mysql from "mysql2/promise";
 const app = express();
 const port = 3001;
 
 app.use(cors({ origin: "http://localhost:3000" }));
 
-const sampleData = [
-    { id: 1, text: "牛乳を買う"},
-    { id: 2, text: "洗濯をする"},
-    { id: 3, text: "請求書を支払う"},
-];
+let myTwitterList: MyTwitter[] = [];
 
-let myTwitterList: MyTwitter[] = sampleData;
+async function main() {
+    const con = await mysql.createConnection({
+        host: "172.29.0.2", //dockerのMySQLコンテナのIPアドレス
+        user: "root",
+        password: "root",
+        database: "typescript_app_db",
+    });
+    const [rows] = await con.execute('SELECT * FROM texts');
+    console.log(rows);
+    await con.end();
+    return rows as MyTwitter[];
+}
 
-app.get("/", (req: Request, res: Response) => {
-    res.json(myTwitterList);
+app.get("/", async (req: Request, res: Response) => {
+    try {
+        myTwitterList = await main();
+        res.status(200).json(myTwitterList);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(null);
+    }
 });
 
 app.listen(port, () => {
