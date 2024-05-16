754fe6e7204071644e2231f2e67ebd780edd7527
ReoChiyomaru <reo.chiyomaru@gmail.com>
2024-05-17 00:31:17 +0900

    seedの初期値を再設定

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -2,13 +2,15 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-
-    const init_text = await prisma.text.create({
-        data: {
-            text: 'こんにちは',
-        },
-    });
-    console.log({ init_text })
+    const texts = ['やっほー', 'うほうほ', 'っぽぽぽっぽー', 'わーい']
+    for(const text of texts){
+        const init_text = await prisma.text.create({
+            data: {
+                text: text,
+            },
+        });
+        console.log({ init_text })
+    }
 }
 
 main()
